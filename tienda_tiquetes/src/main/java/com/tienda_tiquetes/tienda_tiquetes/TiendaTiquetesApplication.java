aa195fefbde58698716120df6ef269a1180a65bc
FranzPfeiffer <FPFEIFFER40204@ufide.ac.cr>
2022-11-09 08:01:19 -0600

    se crea el proyecto tienda_tiquete

---
+++ tienda_tiquetes/src/main/java/com/tienda_tiquetes/tienda_tiquetes/TiendaTiquetesApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_tiquetes.tienda_tiquetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTiquetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTiquetesApplication.class, args);
+	}
+
+}
---
+++ tienda_tiquetes/src/test/java/com/tienda_tiquetes/tienda_tiquetes/TiendaTiquetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_tiquetes.tienda_tiquetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTiquetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
