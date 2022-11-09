ffa618b0591759b4bb8e708fb67f3f4b67e905b0
FranzPfeiffer <98659669+FranzPfeiffer@users.noreply.github.com>
2022-11-09 10:37:57 -0600

    Delete com.tienda_tienda_jar_1 directory
    por alguna razon se colo un archivo de la practica 3 en nuestro git hub, por lo que procedi a eliminar ese folder que no es relevante para nuestro proyecto

--- com.tienda_tienda_jar_1/src/main/java/com/practica/dao/EstadoDao.java
+++
@@ -1,9 +0,0 @@
-package com.practica.dao;
-
-import com.practica.domain.Estado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EstadoDao extends JpaRepository<Estado, Long>{
-    
-}
